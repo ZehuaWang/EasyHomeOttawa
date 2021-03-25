33f9d2849d1590b5e21ad56f5dd0784e9fb85568
ZehuaWang <transforminottawa@gmail.com>
2021-03-24 21:32:25 -0400

    Create system config and user config also initialize the browser instance

--- crawler_engine/realtor_search.js
+++ crawler_engine/realtor_search.js
@@ -1,5 +1,18 @@
-// 引入puppeteer依赖
+// 引入puppeteer-extra依赖
 const puppeteerExtra = require('puppeteer-extra');
 const pluginStealth = require('puppeteer-extra-plugin-stealth');
 const util_tool = require("./util");
+const user_config = require("./user_config");
 
+(async () => 
+    {
+        // 设置Stealth插件
+        puppeteerExtra.use(pluginStealth());
+
+        // 获取浏览器实例 并打开新的Tab
+        const browser = await puppeteerExtra.launch({ headless: false });
+        const page = await browser.newPage();
+
+        // 执行结束后 关闭浏览器
+        await browser.close();
+    })();
---
+++ crawler_engine/sys_config.js
@@ -0,0 +1,1 @@
+exports.realtor="https://www.realtor.ca/en";
