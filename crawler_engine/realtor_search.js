bbe209f56cef4f6c3084fb0a0631509b477ee2ee
ZehuaWang <transforminottawa@gmail.com>
2021-03-24 21:25:13 -0400

    Introduce the dependency and create a util js file

---
+++ crawler_engine/realtor_search.js
@@ -0,0 +1,5 @@
+// 引入puppeteer依赖
+const puppeteerExtra = require('puppeteer-extra');
+const pluginStealth = require('puppeteer-extra-plugin-stealth');
+const util_tool = require("./util");
+
---
+++ crawler_engine/util.js
@@ -0,0 +1,7 @@
+exports.wait = function(ms) {
+    var start = new Date().getTime();
+    var end = start;
+    while (end < start + ms) {
+      end = new Date().getTime();
+    }
+}
